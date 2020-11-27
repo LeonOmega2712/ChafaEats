1c67f3980c13906662f9b4e7aa919ebd27d30bb9
Jonathan Muñoz <jonathan2712emr@gmail.com>
2020-11-27 15:32:06 -0600

    [ADD] Componente de card terminado
    Se modifico el aspecto y funcionamiento

--- src/app/comida-card/comida-card.component.ts
+++ src/app/comida-card/comida-card.component.ts
@@ -12,5 +12,42 @@ export class ComidaCardComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
+  platillosCatalogo = [
+    {
+      titulo: "Tacos al pastor",
+      descripcion: "Orden de 5 tacos mas refresco",
+      urlImg: "../../assets/img/tacos al pastor.jpg",
+      precio: "$50"
+    },
+    {
+      titulo: "Tacos de bistec",
+      descripcion: "Orden de 4 tacos",
+      urlImg: "../../assets/img/tacos de bistec.jpg",
+      precio: "$44"
+    },
+    {
+      titulo: "Huaraches",
+      descripcion: "Orden de 2 mas refresco",
+      urlImg: "../../assets/img/huaraches.jpg",
+      precio: "$40"
+    },
+    {
+      titulo: "Quesadilla",
+      descripcion: "Tortilla de harina, queso y guisado",
+      urlImg: "../../assets/img/Quesadilla.jpg",
+      precio: "$15"
+    },
+    {
+      titulo: "Coca-cola",
+      descripcion: "600 ml",
+      urlImg: "../../assets/img/Coca-cola.jpg",
+      precio: "$15"
+    },
+    {
+      titulo: "Doritos",
+      descripcion: "Sabor nacho 65 g.",
+      urlImg: "../../assets/img/doritos.jpg",
+      precio: "$13"
+    }
+  ]
 }
